931bcd7404d3a845e2947c000919b85133635435
Krows <klushochek15@gmail.com>
2022-06-30 06:18:36 +0300

    fix connection

--- src/main/java/net/krows_team/sticker_bot/StickerBot.java
+++ src/main/java/net/krows_team/sticker_bot/StickerBot.java
@@ -92,9 +92,8 @@ public class StickerBot {
 						HttpURLConnection con = (HttpURLConnection) url.openConnection();
 						con.setRequestMethod("GET");
 						BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-				        String inputLine;
-				        while ((inputLine = in.readLine()) != null) System.out.println(inputLine);
 				        in.close();
+				        con.disconnect();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
