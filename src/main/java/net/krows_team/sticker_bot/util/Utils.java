e9e22eeab3d742cc92bc74dbab37e58a6caca988
Krows <shaga.business@gmail.com>
2022-07-10 10:55:23 +0300

    Changes for tests

--- src/main/java/net/krows_team/sticker_bot/util/Utils.java
+++ src/main/java/net/krows_team/sticker_bot/util/Utils.java
@@ -9,30 +9,36 @@ import com.pengrad.telegrambot.request.GetFile;
 import com.pengrad.telegrambot.response.GetFileResponse;
 
 public class Utils {
-	
+
 	public static String getURLById(TelegramBot api, String id) {
 		GetFile request = new GetFile(id);
 		GetFileResponse response = api.execute(request);
 		return api.getFullFilePath(response.file());
 	}
-	
+
 	public static String getResourcePath(String file) {
 		return "src/main/resources/" + file;
 	}
-	
+
 	public static String getTestResourcePath(String file) {
 		return "src/test/resources/" + file;
 	}
-	
+
+	public static File getOutputFile(String file) {
+		return new File("out/" + file);
+	}
+
 	public static boolean contains(String[] a, String v) {
-		for(String s : a) if(s.equals(v)) return true;
+		for (String s : a)
+			if (s.equals(v))
+				return true;
 		return false;
 	}
-	
+
 	public static byte[] readFile(String path) throws IOException {
 		return Files.readAllBytes(new File(path).toPath());
 	}
-	
+
 	public static <T> T before(T t, Runnable run) {
 		run.run();
 		return t;
--- src/test/java/net/krows_team/sticker_bot/StickerRendererTest.java
+++ src/test/java/net/krows_team/sticker_bot/StickerRendererTest.java
@@ -16,58 +16,61 @@ import net.krows_team.sticker_bot.util.TransferableImage;
 import net.krows_team.sticker_bot.util.Utils;
 
 public class StickerRendererTest {
-	
+
 	private static BufferedImage makeSample(String name, String text) {
 		long timestamp = new Date().getTime();
 		Color color = Colors.random().createColor();
-		StickerRenderer r = new StickerRenderer(new StickerData(timestamp, name, text, color, OptionalPair.of(name.toUpperCase().charAt(0) + "")));
+		StickerRenderer r = new StickerRenderer(
+				new StickerData(timestamp, name, text, color, OptionalPair.of(name.toUpperCase().charAt(0) + "")));
 		r.renderMessage();
 		return r.fitImage();
 	}
-	
+
 	public static BufferedImage helloWorldSample() {
 		String msgName = "Ekaterina";
 		String msgText = "Если бы у меня были права, я бы банила, но я женщина, у женщин нет прав даже в чате кнада";
 		return makeSample(msgName, msgText);
 	}
-	
+
 	public static BufferedImage firstEmojiRowSmaple() {
 		String msgName = "Ekaterina";
 		String msgText = "😀😃😄😁😆😅😂🤣🥲☺️😊😇🙂🙃😉😌😍🥰😘😗😙😚😋😛😝😜🤪🤨🧐🤓😎🥸";
 		return makeSample(msgName, msgText);
 	}
-	
+
 	public static BufferedImage multilineEmojiSample() {
 		String msgName = "Ekaterina";
 		String msgText = "😀😃😄😁😆😅😂🤣🥲☺️😊😇🙂🙃😉😌😍🥰😘😗😙😚😋😛😝😜🤪🤨🧐🤓😎🥸";
 		msgText += msgText;
 		return makeSample(msgName, msgText);
 	}
-	
+
 	public static BufferedImage wordWrapSample() {
 		String msgName = "Ekaterina";
 		String msgText = "Что-то в началеСуперМегаДуперУльтраДлинноеСловоИОноСлитное";
 		return makeSample(msgName, msgText);
 	}
-	
+
 	public static BufferedImage textWithEmojiSample() {
 		String msgName = "Ekaterina";
 		String msgText = "😀😃😄😁😆sddcedc😅😂🤣🥲☺️😊😇kjdcs🙂🙃😉😌😍vdsfjk🥰😘😗😙😚😋😛😝😜wejfewio🤪🤨icuwehfwecweuic🧐🤓😎🥸vdsfjk🥰😘😗😙😚😋😛😝😜wejfewio🤪🤨icuwehfwecweuic🧐🤓😎🥸vdsfjk🥰😘😗😙😚😋😛😝😜wejfewio🤪🤨icuwehfwecweuic🧐🤓😎🥸vdsfjk🥰😘😗😙😚😋😛😝😜wejfewio🤪🤨icuwehfwecweuic🧐🤓😎🥸vdsfjk🥰😘😗😙😚😋😛😝😜wejfewio🤪🤨icuwehfwecweuic🧐🤓😎🥸";
 		return makeSample(msgName, msgText);
 	}
-	
+
 	public static BufferedImage createSample(Supplier<BufferedImage> s) {
 		return s.get();
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		Supplier<BufferedImage> sample = StickerRendererTest::helloWorldSample;
 		BufferedImage sampleImage = createSample(sample);
-		
-		String samplePath = Utils.getResourcePath("tmp.png");
-		ImageIO.write(sampleImage, "png", new File(samplePath));
-		
-		if(Utils.contains(args, "show")) Desktop.getDesktop().open(new File(samplePath));
-		if(Utils.contains(args, "copy")) Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new TransferableImage(sampleImage), null);
+
+		File sampleFile = Utils.getOutputFile("tmp.png");
+		ImageIO.write(sampleImage, "png", sampleFile);
+
+		if (Utils.contains(args, "show"))
+			Desktop.getDesktop().open(sampleFile);
+		if (Utils.contains(args, "copy"))
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new TransferableImage(sampleImage), null);
 	}
 }
